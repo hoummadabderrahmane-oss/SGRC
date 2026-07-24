ee9ef0b5db9571b09101e86b4112d638359bcf8a
Abderrahmane Hoummad <hoummadabderrahmane@gmail.com>
2026-07-24 10:18:36 +0000

    Initial project structure

--- config/database.php
+++ config/database.php
@@ -29,4 +29,4 @@ class Database
 
                                                                                                                                                                                 return $this->conn;
                                                                                                                                                                                     }
-                                                                                                                                                                                    }
+                                                                                                                                                                                    } 
